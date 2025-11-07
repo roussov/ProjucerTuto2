fdbaf882457c687c1dde98314bbe23e2d095038c
Vincent Rousseau <vincent.rousseau.pro@gmail.com>
2025-11-07 14:34:10 +0100

    Clean up header file by removing comments and metadata
    Removed license comments and metadata from the header file.

--- Source/PlayingSoundFilesTutorial_01.h
+++ Source/PlayingSoundFilesTutorial_01.h
@@ -1,53 +1,6 @@
-/*
-  ==============================================================================
-
-   This file is part of the JUCE tutorials.
-   Copyright (c) 2020 - Raw Material Software Limited
-
-   The code included in this file is provided under the terms of the ISC license
-   http://www.isc.org/downloads/software-support-policy/isc-license. Permission
-   To use, copy, modify, and/or distribute this software for any purpose with or
-   without fee is hereby granted provided that the above copyright notice and
-   this permission notice appear in all copies.
-
-   THE SOFTWARE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY, AND ALL WARRANTIES,
-   WHETHER EXPRESSED OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR
-   PURPOSE, ARE DISCLAIMED.
-
-  ==============================================================================
-*/
-
-/*******************************************************************************
- The block below describes the properties of this PIP. A PIP is a short snippet
- of code that can be read by the Projucer and used to generate a JUCE project.
-
- BEGIN_JUCE_PIP_METADATA
-
- name:             PlayingSoundFilesTutorial
- version:          1.0.0
- vendor:           JUCE
- website:          http://juce.com
- description:      Plays audio files.
-
- dependencies:     juce_audio_basics, juce_audio_devices, juce_audio_formats,
-                   juce_audio_processors, juce_audio_utils, juce_core,
-                   juce_data_structures, juce_events, juce_graphics,
-                   juce_gui_basics, juce_gui_extra
- exporters:        xcode_mac, vs2019, linux_make
-
- type:             Component
- mainClass:        MainContentComponent
-
- useLocalCopy:     1
-
- END_JUCE_PIP_METADATA
-
-*******************************************************************************/
-
-
 #pragma once
 
-//==============================================================================
+
 class MainContentComponent   : public juce::AudioAppComponent,
                                public juce::ChangeListener
 {
@@ -140,22 +93,22 @@ private:
 
             switch (state)
             {
-                case Stopped:                           // [3]
+                case Stopped:                           
                     stopButton.setEnabled (false);
                     playButton.setEnabled (true);
                     transportSource.setPosition (0.0);
                     break;
 
-                case Starting:                          // [4]
+                case Starting:                          
                     playButton.setEnabled (false);
                     transportSource.start();
                     break;
 
-                case Playing:                           // [5]
+                case Playing:                           
                     stopButton.setEnabled (true);
                     break;
 
-                case Stopping:                          // [6]
+                case Stopping:                          
                     transportSource.stop();
                     break;
             }
@@ -166,24 +119,24 @@ private:
     {
         chooser = std::make_unique<juce::FileChooser> ("Select a Wave file to play...",
                                                        juce::File{},
-                                                       "*.wav");                     // [7]
+                                                       "*.wav");                     
         auto chooserFlags = juce::FileBrowserComponent::openMode
                           | juce::FileBrowserComponent::canSelectFiles;
 
-        chooser->launchAsync (chooserFlags, [this] (const juce::FileChooser& fc)     // [8]
+        chooser->launchAsync (chooserFlags, [this] (const juce::FileChooser& fc)     
         {
             auto file = fc.getResult();
 
-            if (file != juce::File{})                                                // [9]
+            if (file != juce::File{})                                                
             {
-                auto* reader = formatManager.createReaderFor (file);                 // [10]
+                auto* reader = formatManager.createReaderFor (file);                 
 
                 if (reader != nullptr)
                 {
-                    auto newSource = std::make_unique<juce::AudioFormatReaderSource> (reader, true);   // [11]
-                    transportSource.setSource (newSource.get(), 0, nullptr, reader->sampleRate);       // [12]
-                    playButton.setEnabled (true);                                                      // [13]
-                    readerSource.reset (newSource.release());                                          // [14]
+                    auto newSource = std::make_unique<juce::AudioFormatReaderSource> (reader, true);   
+                    transportSource.setSource (newSource.get(), 0, nullptr, reader->sampleRate);       
+                    playButton.setEnabled (true);                                                      
+                    readerSource.reset (newSource.release());                                          
                 }
             }
         });
@@ -199,7 +152,7 @@ private:
         changeState (Stopping);
     }
 
-    //==========================================================================
+    
     juce::TextButton openButton;
     juce::TextButton playButton;
     juce::TextButton stopButton;
@@ -213,3 +166,4 @@ private:
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
 };
+
